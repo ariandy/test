8fd98cdabb88ebcb5ccfb7e4e45c2fc953658d36
Ariandy Noviar <ariandy.noviar@modalrakyat.com>
2019-11-05 20:34:50 +0700

    Change multiplier constant

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 fn main() {
-    // prng(&mut 3,4,7,15);
-    // prng(&mut 2,2,7,10);
+    prng(&mut 3,4,7,15);
+    prng(&mut 2,2,7,10);
     println!("{}", i32::max_value());
-    pml(&mut 11111111)
+    println!("{}", 2u32.pow(31)-1);
+    pml(&mut 11);
 }
 
 fn prng(seed: &mut i32, mult: i32, inc: i32, m: i32) {
@@ -12,14 +13,14 @@ fn prng(seed: &mut i32, mult: i32, inc: i32, m: i32) {
     }
 }
 
-fn lcg(seed: &mut i32, a: i32, m: i32) {
-    for _x in 0..10 {
+fn lcg(seed: &mut i32, a: i32, m: i32, repeat: i8) {
+    for _x in 0..repeat {
         *seed = (a * *seed) % m;
         println!("{:?}", seed)
     }
 }
 
 fn pml(seed: &mut i32) {
-    lcg(&mut 726347, 7^5, i32::max_value()-1)
-    //according to PRIMOS, IMSL, SIMPL/I, APL, 
+    lcg(seed, 7i32.pow(5), i32::max_value(), 1)
+    //according to PRIMOS, IMSL, SIMPL/I, APL
 }
