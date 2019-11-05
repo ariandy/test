e864bbdb9f8630a3b52ddf88b6194b098cea6900
Ariandy Noviar <ariandy.noviar@modalrakyat.com>
2019-11-05 20:09:20 +0700

    Add Park-Miller-Lehmer Generator

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 fn main() {
-    prng(&mut 3,4,7,15);
-    prng(&mut 2,2,7,10);
+    // prng(&mut 3,4,7,15);
+    // prng(&mut 2,2,7,10);
+    println!("{}", i32::max_value());
+    pml(&mut 11111111)
 }
 
 fn prng(seed: &mut i32, mult: i32, inc: i32, m: i32) {
@@ -15,4 +17,9 @@ fn lcg(seed: &mut i32, a: i32, m: i32) {
         *seed = (a * *seed) % m;
         println!("{:?}", seed)
     }
+}
+
+fn pml(seed: &mut i32) {
+    lcg(&mut 726347, 7^5, i32::max_value()-1)
+    //according to PRIMOS, IMSL, SIMPL/I, APL, 
 }
